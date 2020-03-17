47ff9a7d7097734fa259f1ea6adfaef0ffe7530c
Saba Khan <saba100.ksk@gmail.com>
2020-03-17 12:36:28 +0530

    minor changes

--- library_app/lib/Screens/registerPage.dart
+++ library_app/lib/Screens/registerPage.dart
@@ -16,15 +16,17 @@ class _RegisterPageState extends State<RegisterPage> {
         backgroundColor: Colors.black,
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.only(top: 70.0),
+        padding: EdgeInsets.only(top: 50.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             InputField(placeholder: "Name"),
             SizedBox(height: 15.0),
-            InputField(placeholder: "Mobile Number"),
-            SizedBox(height: 15.0),
             InputField(placeholder: "ID"),
+            SizedBox(height: 15.0),
+            InputField(placeholder: "Email"),
+            SizedBox(height: 15.0),
+            InputField(placeholder: "Mobile Number"),
             SizedBox(height: 15.0),
             InputField(placeholder: "New Password", boolValue: "hide"),
             SizedBox(height: 15.0),
